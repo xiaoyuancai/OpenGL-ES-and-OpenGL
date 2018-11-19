fe9f2b899bd3f6d6a98b92a0dc92214a8ee1e46d
xiaoyuancai <18101372070@163.com>
2018-11-19 14:51:54 +0800

    修改注释

--- openglES/openglES/ViewController.m
+++ openglES/openglES/ViewController.m
@@ -54,28 +54,25 @@ static const SceneVertex vertices[] =
     //生成，绑定，初始化GPU缓存内容，因为数据要发送到GPU来渲染
     glGenBuffers(1,//指定要生成的缓存标识符数量
                  &vertexBufferID//指向生成的标识符的内存保存位置
-                 );//STEP 1 生成标识符
+                 );//STEP 1 生成一个顶点缓存对象
     
     glBindBuffer(GL_ARRAY_BUFFER,//常量，缓存类型，此处是一个数组，还有一种类型后面说明
-                 vertexBufferID);//STEP 2 绑定缓存
+                 vertexBufferID);//STEP 2 新创建的缓冲绑定到GL_ARRAY_BUFFER目标上
     
-    glBufferData(GL_ARRAY_BUFFER,//指定要更新当前上下文中所绑定的是哪一个缓存
+    glBufferData(GL_ARRAY_BUFFER,//目标缓冲的类型：顶点缓冲对象当前绑定到
+                                //GL_ARRAY_BUFFER目标上
                  sizeof(vertices),//要复制进这个缓存的字节的数量(大小)
-                 vertices,//要复制的字节地址(数据)
-                 GL_STATIC_DRAW//提示了缓存在未来的运算中可能将会被怎样使用，
-                                //GL_STATIC_DRAW会告诉上下文，缓存的内容适合复制到GPU控制的内存，因为很少对其它进行修改，这个可以帮助OpenGL优化内存。使用GL_DYNAMIC_DRAW，缓存内的数据会频繁改变，同时提示OpenGLES以不同的方式来处理缓存的存储
-                 );//STEP 3 复制应用的顶点数据到当前上下文的所绑定的顶点缓存中
+                 vertices,//要复制的字节地址(希望发送的实际数据)
+                 GL_STATIC_DRAW//希望显卡如何管理给定的数据，
+                                //GL_STATIC_DRAW数据不会或几乎不会改变。这个可以帮助OpenGL优化内存。
+                 );//STEP 3 把之前定义的顶点数据复制到缓冲的内存中
     
     /**
-     补充：什么时候用 GL_STATIC_DRAW，什么时候用GL_STREAM_DRAW
+     GL_STATIC_DRAW ：数据不会或几乎不会改变。
+     GL_DYNAMIC_DRAW：数据会被改变很多。
+     GL_STREAM_DRAW ：数据每次绘制时都会改变。
      
-     STREAM
-     You should use STREAM_DRAW when the data store contents will be modified once and used at most a few times.
-     
-     STATIC
-     Use STATIC_DRAW when the data store contents will be modified once and used many times.
-     
-     答案来源：https://stackoverflow.com/questions/8281653/how-to-choose-between-gl-stream-draw-or-gl-dynamic-draw
+     比如说一个缓冲中的数据将频繁被改变，那么使用的类型就是GL_DYNAMIC_DRAW或GL_STREAM_DRAW，这样就能确保显卡把数据放在能够高速写入的内存部分。
      
      */
     
@@ -95,9 +92,9 @@ static const SceneVertex vertices[] =
     glVertexAttribPointer(GLKVertexAttribPosition,//当前绑定的缓存包含每个顶点的位置信息
                           3,//每个位置有三部分
                           GL_FLOAT,//每个部分都保存了float类型的数据
-                          GL_FALSE,//小数点固定数据是否可以被改变
+                          GL_FALSE,//是否希望数据被标准化(Normalize)。如果我们设置为GL_TRUE，所有数据都会被映射到0（对于有符号型signed数据是-1）到1之间。我们把它设置为GL_FALSE
                           sizeof(SceneVertex),//"步幅"，每个顶点的保存需要多少个字节(指定了GUP从一个顶点的内存开始位置转到下一个顶点的内存开始位置需要跳过多少字节)
-                          NULL);//从当前绑定的顶点缓存的开始位置访问顶点数据
+                          NULL);//从当前绑定的顶点缓存的开始位置访问顶点数据(表示位置数据在缓冲中起始位置的偏移量)
     
     //STEP 6绘图
     glDrawArrays(GL_TRIANGLES,//如何去处理顶点缓存内的顶点数据，此处是去渲染三角形
